ef91eb8c7f0776d4c1f59ada10a5adba747e073b
kkb568 <b.koome@alustudent.com>
2022-11-27 19:52:09 +0400

    6th commit

--- page.js
+++ page.js
@@ -2,5 +2,7 @@ function moveElement(current, next) {
     var current = document.getElementById(current);
     var next = document.getElementById(next);
     current.style.opacity = "0";
+    current.style.zIndex = "-1"
     next.style.opacity = "1";
+    next.style.zIndex  = "1";
 }
