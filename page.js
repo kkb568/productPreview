8690481cc69e23d425b18d5b8c2f6e7cb3c9bfd4
kkb568 <b.koome@alustudent.com>
2022-11-29 01:57:50 +0400

    7th commit

--- page.js
+++ page.js
@@ -6,3 +6,17 @@ function moveElement(current, next) {
     next.style.opacity = "1";
     next.style.zIndex  = "1";
 }
+
+function openModal(modal,content) {
+    var Modal = document.getElementById(modal);
+    var Content = document.getElementById(content);
+    Modal.style.display = 'block';
+    Content.style.display = 'block';
+}
+
+function closeModal(modal,content) {
+    var Modal = document.getElementById(modal);
+    var Content = document.getElementById(content);
+    Modal.style.display = 'none';
+    Content.style.display = 'none';
+}
